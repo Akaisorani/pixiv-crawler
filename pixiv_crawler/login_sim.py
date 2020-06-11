850215980fea57fcfc60b2372461fcb2b270d441
“Akaisorani” <1224067801@qq.com>
2020-06-11 20:53:52 +0800

    upd log sim

--- pixiv_crawler/login_sim.py
+++ pixiv_crawler/login_sim.py
@@ -44,12 +44,12 @@ def login_for_cookies(config):
         # WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"iframe[name^='a-'][src^='https://www.google.com/recaptcha/api2/anchor?']")))
         # WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[@class='recaptcha-checkbox goog-inline-block recaptcha-checkbox-unchecked rc-anchor-checkbox']/div[@class='recaptcha-checkbox-checkmark']"))).click()
         element = WebDriverWait(driver, 60).until(
-                EC.presence_of_element_located((By.ID, "page-mypage"))
+                EC.presence_of_element_located((By.ID, "root"))
             )
     except Exception as e:
         driver.save_screenshot("./temp/log_in_err.png")
         driver.quit()
-        raise IOError("login sim wait failed, page-mypage did not appear")
+        raise IOError("login sim wait failed, 'root' did not appear")
     
     cookies_dict=dict()
     cookies=driver.get_cookies()
@@ -67,7 +67,8 @@ if __name__=="__main__":
             self.password=""
             self.proxies_enable = True
             self.socks = ""
-            self.phantomjs = "D:/tectree/Code/phantomjs-2.1.1-windows/bin/phantomjs.exe"
+            # self.phantomjs = "D:/tectree/Code/phantomjs-2.1.1-windows/bin/phantomjs.exe"
+            self.phantomjs = "/Users/yuehan/tectree/Code/phantomjs-2.1.1-macosx/bin/phantomjs"
             self.firefox=""
             self.chrome=""
             
